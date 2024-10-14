9a4fd1d4d054f646454dfbec0423fb4fbd7bcc0d
Anurag Choudhary <anurag.choudhary01@sap.com>
2024-10-14 14:09:02 +0530

    Added comments

--- gitHub-api/test-gitHub-api.sh
+++ gitHub-api/test-gitHub-api.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+################
+
+# Author: Anurag
+# Date : 14 OCT 2024
+# A script to use gitHub apis via shell script
+
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -8,8 +14,8 @@ USERNAME=$username
 TOKEN=$token
 
 # User and Repository information
-REPO_OWNER=$1
-REPO_NAME=$2
+REPO_OWNER=$1 # repo owner name ex: https://github.com/Suii2023/basicCode : Suii2023
+REPO_NAME=$2 # repo name : basicCode
 
 # Function to make a GET request to the GitHub API
 function github_api_get {
@@ -36,7 +42,21 @@ function list_users_with_read_access {
     fi
 }
 
+function list_pull_requests {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/pulls"
+
+    pullrequests="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login'))"
+
+    if [[ -z $pullrequests ]]; then
+        wcho "no pull reuest found for ${REPO_OWNER}/${REPO_NAME}."
+    else
+        echo "pull requests for Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo $pullrequests
+    fi
+}
+
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
-list_users_with_read_access
+list_users_with_read_access;
+list_pull_requests
