ecedef2dcb3d6816949674ddb314799e8681bd9a
Anurag Choudhary <anurag.choudhary01@sap.com>
2024-10-14 14:33:05 +0530

    included fetching full requests as well

--- gitHub-api/test-gitHub-api.sh
+++ gitHub-api/test-gitHub-api.sh
@@ -5,6 +5,14 @@
 # Author: Anurag
 # Date : 14 OCT 2024
 # A script to use gitHub apis via shell script
+# jq -r tells to give the raw output as palin text
+# The -n flag checks if a string is not empty (i.e., has a length greater than zero).
+# The -z flag in a conditional statement checks if a string is empty. Specifically, it returns true if the string has a length of zero.
+# Use -z to check if a string is empty.
+# Use -n to check if it is not empty.
+#Direct comparisons and the test command can also be utilized for similar checks. EX: if test -z "$variable"; then
+# echo "The variable is empty."
+
 
 # GitHub API URL
 API_URL="https://api.github.com"
@@ -45,13 +53,14 @@ function list_users_with_read_access {
 function list_pull_requests {
     local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/pulls"
 
-    pullrequests="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login'))"
+    pullrequests="$(github_api_get "$endpoint" | jq -r '.[] | {title: .title, number: .number, state: .state}')"
 
-    if [[ -z $pullrequests ]]; then
-        wcho "no pull reuest found for ${REPO_OWNER}/${REPO_NAME}."
+    if [[ -z "$pullrequests" ]]; then
+        echo "no pull requests found for ${REPO_OWNER}/${REPO_NAME}."
+        echo "$pullrequests"
     else
-        echo "pull requests for Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
-        echo $pullrequests
+        echo "pull requests to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "$pullrequests"
     fi
 }
 
