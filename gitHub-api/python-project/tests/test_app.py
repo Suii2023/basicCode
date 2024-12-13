c172269921f19c0c6b7bd1dbe10c0ccf37af2328
Anurag Choudhary <anurag.choudhary01@sap.com>
2024-12-13 10:34:21 +0530

    Testing the workflow

--- gitHub-api/python-project/tests/test_app.py
+++ gitHub-api/python-project/tests/test_app.py
@@ -8,10 +8,19 @@ def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
-
+    
 def test_hello(client):
     with patch("app.render_template") as mock_render_template:
         mock_render_template.return_value = "Rendered template content"  # mock return for the test 
+
+    #with patch("app.hello") as mocked_method:
+        #mocked_method.return_value = mock_render_template
     response = client.get('/')
     assert response.status_code == 200
+
+    #mocked assert not working 
+    #mocked_method.assert_called_once()
+    #mock_render_template.assert_called_once_with('index.html')
+
+
     #mock_render_template.assert_called_once_with('index.html')
